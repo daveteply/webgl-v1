95a9bc810b329a9886e1f680763981626d5f83c3
David Teply <davehamdan@gmail.com>
2021-10-10 06:27:36 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wgl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'webgl-v1';
+}
