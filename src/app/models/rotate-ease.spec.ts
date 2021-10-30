38abce35b0e3942fc416dad5c3c92afa4fca1634
David Teply <davehamdan@gmail.com>
2021-10-30 07:21:26 -0700

    fixing tests

--- src/app/models/rotate-ease.spec.ts
+++ src/app/models/rotate-ease.spec.ts
@@ -1,7 +1,11 @@
 import { RotateEase } from './rotate-ease';
 
 describe('RotateEase', () => {
+  const mockStart = 1.3;
+  const mockEnd = 4.2;
+  const mockSteps = 4;
+
   it('should create an instance', () => {
-    expect(new RotateEase()).toBeTruthy();
+    expect(new RotateEase(mockStart, mockEnd, mockSteps)).toBeTruthy();
   });
 });
