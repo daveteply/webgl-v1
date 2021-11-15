a0dd6a12288ccb8125547cf193d5d2a484b1156d
Dave Teply <davehamdan@gmail.com>
2021-11-15 16:21:30 -0700

    fine tune easing

--- src/app/models/rotate-ease.ts
+++ src/app/models/rotate-ease.ts
@@ -1,23 +1,18 @@
 import { MathUtils } from 'three';
 
 export class RotateEase {
-  private _currentTheta: number;
-  private _targetTheta: number;
   private _steps: number[] = [];
-
   private _iterator: number = 0;
 
   constructor(currentTheta: number, targetTheta: number, steps: number) {
-    this._currentTheta = currentTheta;
-    this._targetTheta = targetTheta;
-
     // establish steps
     const inc = 1 / steps;
     for (let i = inc; i <= inc * steps; i += inc) {
-      this._steps.push(
-        MathUtils.lerp(this._currentTheta, this._targetTheta, i)
-      );
+      this._steps.push(MathUtils.lerp(currentTheta, targetTheta, i));
     }
+
+    // ensure final element is target theta
+    this._steps[steps - 1] = targetTheta;
   }
 
   public get HasNext(): boolean {
