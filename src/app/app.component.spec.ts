29f6ae1249314111253bea1ca5db958fbd6bb8f2
Dave Teply <davehamdan@gmail.com>
2021-11-20 02:58:52 -0700

    fix console error during test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, CanvasComponent],
     }).compileComponents();
   });
 
