c8bbc62ca0927e27c1c000af9732d27855320f19
Dave Teply <davehamdan@gmail.com>
2021-12-09 01:57:19 -0700

    upgrade to angular 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, HammerModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './canvas/canvas.component';
 
 @NgModule({
   declarations: [AppComponent, CanvasComponent],
-  imports: [BrowserModule, HammerModule],
+  imports: [BrowserModule, AppRoutingModule, HammerModule],
   providers: [],
   bootstrap: [AppComponent],
 })
