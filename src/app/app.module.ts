1bb2759d6be7a98a1336cdc7b2fd28bd5191ec64
Dave Teply <davehamdan@gmail.com>
2021-12-10 03:09:04 -0700

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './canvas/canvas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, CanvasComponent],
-  imports: [BrowserModule, AppRoutingModule, HammerModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HammerModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
