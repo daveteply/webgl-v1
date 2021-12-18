79279c1dbc0b0fdfa6f1bb9d09b34ff3f6ac2574
Dave Teply <davehamdan@gmail.com>
2021-12-18 08:26:29 -0700

    eslint

--- src/app/intro/components/intro-container/intro-container.component.ts
+++ src/app/intro/components/intro-container/intro-container.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'wgl-intro-container',
   templateUrl: './intro-container.component.html',
   styleUrls: ['./intro-container.component.scss'],
 })
-export class IntroContainerComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class IntroContainerComponent {}
