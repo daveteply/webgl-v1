a9cfd1587f10e54788eaa840b3c081a4aaf54dc8
Dave Teply <davehamdan@gmail.com>
2022-02-18 04:50:17 -0700

    update intro instructions

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -21,6 +21,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatDialogModule,
     MatButtonModule,
     MatExpansionModule,
+    MatIconModule,
   ],
   providers: [
     SceneManagerService,
