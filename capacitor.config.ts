1e4d8a9e74935a30352b86512ba9d2aa4814cf07
Dave Teply <davehamdan@gmail.com>
2022-03-27 12:05:11 -0700

    add capacitor android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.daveteply.webgl01',
+  appName: 'webgl-v1',
+  webDir: 'dist/webgl-v1',
+  bundledWebRuntime: false
+};
+
+export default config;
