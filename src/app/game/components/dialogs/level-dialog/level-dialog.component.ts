2687255b032e7502ce9b6d145d299e4602dca86b
Dave Teply <davehamdan@gmail.com>
2022-05-06 04:49:14 -0700

    adding header/footer animation to level dialog

--- src/app/game/components/dialogs/level-dialog/level-dialog.component.ts
+++ src/app/game/components/dialogs/level-dialog/level-dialog.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Inject, OnDestroy } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Inject, OnDestroy, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { delay } from 'rxjs';
 import { GAME_TITLE, MINIMUM_MATCH_COUNT } from 'src/app/game/game-constants';
 import { TextureManagerService } from 'src/app/game/services/texture/texture-manager.service';
 import { AudioType } from 'src/app/shared/services/audio/audio-info';
 import { AudioManagerService } from 'src/app/shared/services/audio/audio-manager.service';
+import { MathUtils } from 'three';
 import { LevelDialogData } from './level-dialog-data';
 
 enum LevelElementType {
@@ -20,12 +21,20 @@ interface LevelStat {
   statValue: number;
 }
 
+interface boxParticle {
+  x: number;
+  y: number;
+  size: number;
+  velocity: number;
+  limit: number;
+}
+
 @Component({
   selector: 'wgl-level-dialog',
   templateUrl: './level-dialog.component.html',
   styleUrls: ['./level-dialog.component.scss'],
 })
-export class LevelDialogComponent implements OnDestroy {
+export class LevelDialogComponent implements OnDestroy, AfterViewInit {
   matchTarget = MINIMUM_MATCH_COUNT;
   texturesStillLoading: boolean = true;
   progress: number = 100;
@@ -41,6 +50,14 @@ export class LevelDialogComponent implements OnDestroy {
   timerEvent: EventEmitter<LevelStat> = new EventEmitter<LevelStat>();
 
   gameTitle = GAME_TITLE;
+
+  @ViewChild('dialogCanvas')
+  dialogCanvas!: ElementRef<HTMLCanvasElement>;
+
+  canvas!: HTMLCanvasElement;
+  ctx!: CanvasRenderingContext2D | null;
+  boxesTop!: boxParticle[];
+  boxesBottom!: boxParticle[];
 
   constructor(
     private textureManager: TextureManagerService,
@@ -92,6 +109,10 @@ export class LevelDialogComponent implements OnDestroy {
     });
 
     this.setData(data);
+  }
+
+  ngAfterViewInit(): void {
+    this.setScene();
   }
 
   ngOnDestroy(): void {
@@ -156,5 +177,72 @@ export class LevelDialogComponent implements OnDestroy {
         this.timerEvent.next(nextElement);
       }
     }
+  }
+
+  private setScene(): void {
+    this.canvas = this.dialogCanvas.nativeElement;
+    // resize canvas to match css size
+    this.canvas.width = this.canvas.clientWidth;
+    this.canvas.height = this.canvas.clientHeight;
+
+    this.ctx = this.canvas.getContext('2d');
+    if (this.ctx) {
+      this.ctx.globalAlpha = 0.5;
+
+      // create boxes
+      this.boxesTop = [];
+      this.boxesBottom = [];
+      for (let i = 0; i < 20; i++) {
+        this.boxesTop.push(this.createBox());
+        this.boxesBottom.push(this.createBox(true));
+      }
+
+      this.animate();
+    }
+  }
+
+  private createBox(isBottom: boolean = false): boxParticle {
+    const size = MathUtils.randInt(5, 30);
+    const x = MathUtils.randInt(0, this.canvas.width);
+    const velocity = MathUtils.randFloat(0.1, 0.3);
+
+    let y = -size;
+    let limit = MathUtils.randInt(4, 35);
+    if (isBottom) {
+      y = this.canvas.height;
+      limit = this.canvas.height - (limit + size);
+    }
+
+    return { size, x, y, velocity, limit };
+  }
+
+  private animate(): void {
+    if (this.ctx) {
+      this.ctx.fillStyle = 'rgba(255,255,255,0.1)';
+      this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+      // box lengths are the same
+      for (let i = 0; i < this.boxesTop.length; i++) {
+        // top
+        this.boxesTop[i].y += this.boxesTop[i].velocity;
+        if (this.boxesTop[i].y > this.boxesTop[i].limit) {
+          this.boxesTop[i] = this.createBox();
+        }
+        const boxTop = this.boxesTop[i];
+        this.ctx.strokeRect(boxTop.x, boxTop.y, boxTop.size, boxTop.size);
+
+        // bottom
+        this.boxesBottom[i].y -= this.boxesBottom[i].velocity;
+        if (this.boxesBottom[i].y < this.boxesBottom[i].limit) {
+          this.boxesBottom[i] = this.createBox(true);
+        }
+        const boxBottom = this.boxesBottom[i];
+        this.ctx.strokeRect(boxBottom.x, boxBottom.y, boxBottom.size, boxBottom.size);
+      }
+    }
+
+    requestAnimationFrame(() => {
+      this.animate();
+    });
   }
 }
