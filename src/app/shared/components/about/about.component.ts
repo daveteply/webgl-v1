1b787fcfca9ad69ce1065b00542d0550d879bf66
Dave Teply <davehamdan@gmail.com>
2022-05-24 05:25:48 -0700

    credits

--- src/app/shared/components/about/about.component.ts
+++ src/app/shared/components/about/about.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { EmojiInfo } from 'src/app/app-store/models/emoji-info';
 import { StoreService } from 'src/app/app-store/services/store.service';
+import { Credits } from './credits';
 
 @Component({
   selector: 'wgl-about',
@@ -14,8 +15,12 @@ export class AboutComponent implements OnInit {
 
   hideLevelInfoData: boolean;
 
+  creditsData: any;
+
   constructor(private store: StoreService, @Inject(MAT_DIALOG_DATA) public hideLevelInfo: boolean) {
     this.hideLevelInfoData = hideLevelInfo;
+
+    this.creditsData = Credits.sort((a, b) => (a.order > b.order ? 1 : a.order));
   }
 
   ngOnInit(): void {
