e8a062561464827bae6216a23ad944fb306c264c
Dave Teply <davehamdan@gmail.com>
2022-08-09 05:15:01 -0700

    fixing tests

--- src/app/game/components/dialogs/intro-dialog/intro-dialog.component.spec.ts
+++ src/app/game/components/dialogs/intro-dialog/intro-dialog.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule } from '@angular/material/dialog';
+import { EffectsManagerService } from 'src/app/game/services/effects-manager.service';
+import { GameEngineService } from 'src/app/game/services/game-engine.service';
+import { MaterialManagerService } from 'src/app/game/services/material/material-manager.service';
+import { ObjectManagerService } from 'src/app/game/services/object-manager.service';
+import { ScoringManagerService } from 'src/app/game/services/scoring-manager.service';
 import { TextureManagerService } from 'src/app/game/services/texture/texture-manager.service';
 
 import { IntroDialogComponent } from './intro-dialog.component';
@@ -12,7 +17,14 @@ describe('IntroDialogComponent', () => {
     await TestBed.configureTestingModule({
       imports: [MatDialogModule],
       declarations: [IntroDialogComponent],
-      providers: [TextureManagerService],
+      providers: [
+        ObjectManagerService,
+        MaterialManagerService,
+        TextureManagerService,
+        EffectsManagerService,
+        ScoringManagerService,
+        GameEngineService,
+      ],
     }).compileComponents();
   });
 
