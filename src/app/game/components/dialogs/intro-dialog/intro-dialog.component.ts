96450bb12c3a7a4b3132db8cfdcc55455c4c802e
Dave Teply <davehamdan@gmail.com>
2022-08-17 04:46:04 -0700

    adding cta to intro dialog

--- src/app/game/components/dialogs/intro-dialog/intro-dialog.component.ts
+++ src/app/game/components/dialogs/intro-dialog/intro-dialog.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Subject, takeUntil } from 'rxjs';
 
 import { AboutComponent } from 'src/app/shared/components/about/about.component';
@@ -7,18 +7,21 @@ import { AboutComponent } from 'src/app/shared/components/about/about.component'
 import { AudioManagerService } from 'src/app/shared/services/audio/audio-manager.service';
 import { DialogAnimationService } from '../dialog-animation.service';
 import { ObjectManagerService } from 'src/app/game/services/object-manager.service';
+import { SaveGameService } from 'src/app/game/services/save-game/save-game.service';
 
 @Component({
   selector: 'wgl-intro-dialog',
   templateUrl: './intro-dialog.component.html',
   styleUrls: ['./intro-dialog.component.scss'],
 })
-export class IntroDialogComponent implements AfterViewInit, OnDestroy {
+export class IntroDialogComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('dialogCanvas')
   dialogCanvas!: ElementRef<HTMLCanvasElement>;
 
   materialsUpdating: boolean = true;
   progress: number = 100;
+
+  hasRestoreData!: boolean;
 
   private notifier = new Subject();
 
@@ -26,7 +29,9 @@ export class IntroDialogComponent implements AfterViewInit, OnDestroy {
     private objectManager: ObjectManagerService,
     private audioManager: AudioManagerService,
     private dialogAnimation: DialogAnimationService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private saveGame: SaveGameService,
+    public dialogRef: MatDialogRef<IntroDialogComponent>
   ) {
     this.objectManager.LevelMaterialsUpdated.pipe(takeUntil(this.notifier)).subscribe(() => {
       this.materialsUpdating = false;
@@ -34,6 +39,15 @@ export class IntroDialogComponent implements AfterViewInit, OnDestroy {
 
     // start-up music
     this.audioManager.PlayLevelComplete();
+  }
+
+  ngOnInit(): void {
+    this.saveGame
+      .HasSaveState()
+      .pipe(takeUntil(this.notifier))
+      .subscribe((hasSaveData) => {
+        this.hasRestoreData = hasSaveData;
+      });
   }
 
   ngOnDestroy(): void {
@@ -51,5 +65,10 @@ export class IntroDialogComponent implements AfterViewInit, OnDestroy {
 
   openAbout(): void {
     this.dialog.open(AboutComponent, { data: { hideLevelInfo: true }, panelClass: 'cdk-overlay-pane__show' });
+  }
+
+  restoreGame(): void {
+    this.saveGame.RestoreState();
+    this.dialogRef.close();
   }
 }
