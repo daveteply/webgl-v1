b107470fe7f0782c3647f406a85fee062ae19732
Dave Teply <davehamdan@gmail.com>
2022-08-17 04:45:22 -0700

    adding save service and interfaces

---
+++ src/app/game/services/save-game/save-game-data.ts
@@ -0,0 +1,39 @@
+import { LevelStats } from '../../models/level-stats';
+
+export interface SaveMaterialData {
+  matchKey: number;
+  bumpSrc?: string;
+  textureSrc?: string;
+  colorStr?: string;
+  emojiSequence?: string;
+}
+
+export interface SavePieceData {
+  isRemoved: boolean;
+  flipTurns: number;
+  flipUp: boolean;
+}
+
+export interface SaveWheelData {
+  theta: number;
+  piecesData: SavePieceData[];
+}
+
+export interface SaveGameData {
+  levelMaterialType?: number;
+  levelGeometryType?: number;
+  wheelData: SaveWheelData[];
+  textureData: SaveMaterialData[];
+  gameMaterials?: number[][][];
+  scoring?: SaveGameScore;
+}
+
+export interface SaveGameScore {
+  stats: LevelStats;
+  moves: number;
+  remaining: number;
+  progress: number;
+  pieceTarget: number;
+  score: number;
+  level: number;
+}
