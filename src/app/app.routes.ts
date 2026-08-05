3362fa8f8d126b92eba36e67e90915a8b25a811e
Dave Teply <davehamdan@gmail.com>
2026-08-05 20:02:47 +0000

    arch: adding intital game container

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'game', pathMatch: 'full' },
+  {
+    path: 'game',
+    loadComponent: () =>
+      import('./game/components/game-container/game-container').then((c) => c.GameContainer),
+  },
+];
---
+++ src/app/game/components/game-container/game-container.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wgl-game-container',
+  imports: [],
+  templateUrl: './game-container.html',
+  styleUrl: './game-container.scss',
+})
+export class GameContainer {}
